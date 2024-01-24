0909c19e002f46a0ca6f09ea5d07fae59ffac7ca
Sarah Syed <sarahsyed.fatima@gmail.com>
2024-01-24 09:38:44 +0500

    variables

---
+++ hello.java
@@ -0,0 +1,9 @@
+class hello{
+    public static void main(String[] args) {
+        int num = 5;
+        int num2 = 5;
+        int result = num + num2;
+        System.out.println(result);
+        System.out.println("Hello World!/n");
+}
+}
