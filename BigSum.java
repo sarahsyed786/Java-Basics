acd394d8d00801330bf8947317153ba9c76e55f4
Sarah Syed <sarahsyed.fatima@gmail.com>
2024-02-24 20:27:05 +0500

    PlusMinus
    solved pluusMinus problem using Java.

---
+++ BigSum.java
@@ -0,0 +1,20 @@
+public class BigSum {
+    public static long aVeryBigSum(long[] ar) {
+        long sum = 0l;
+        for( long i : ar)
+        {
+            sum +=i;
+        
+        }
+        System.out.println(sum);
+        return sum;
+}
+
+    public static void  main (String[] args) {
+     
+        long[] nums = {100001,100003,100003};
+       
+        aVeryBigSum(nums);
+    }
+
+}
--- Sorting.java
+++ Sorting.java
@@ -1,50 +1,72 @@
-import java.util.Arrays;
-
-class hash{
-    int[] arr = {3, 10, -2, 5, 7};
-    int[] sArr = {};
-        public void hash_code()
-        {
-           
-           
-            if (arr.length > 0){
-                    sArr = new int[arr.length];
-                    for (int i=0;i<5;i++){
-                        if(i >arr[i]){
-                        sArr[i] = arr[i];
-                        }
-                    }
-        }
-        
-        System.out.println(sArr);
-        }
-
-       
-        public int hashCode() {
-            final int prime = 31;
-            int result = 1;
-            result = prime * result + Arrays.hashCode(arr);
-            result = prime * result + Arrays.hashCode(sArr);
-            return result;
-        }
-
-}
-
-
-
 public class Sorting {
-
+        public static void print(int[] unSortedArray)
+        {
+            for(int sortedArray:unSortedArray)
+            {
+                System.out.print(sortedArray + " ");
+            }
+        }
 
     public static void main(String[] args) {
-       hash obj = new hash();
-       obj.hash_code();
-        
-     
+
+        int[] unSortedArray = {7,8,3,0,1,2,5};
+
+            /* Bubble Sort */
+            /*for(int i=0; i<unSortedArray.length-1; i++)
+            {
+                for(int j =0; j<unSortedArray.length-i-1; j++)
+                {
+                    if(unSortedArray[j] > unSortedArray[j+1])
+                    {
+                        int temp = unSortedArray[j];
+                        unSortedArray[j] = unSortedArray[j+1];
+                        unSortedArray[j+1] = temp;
+                    }
+                }
+            }*/
+
+            //Selection Sort
+
+            /*for(int i=0; i<unSortedArray.length-1; i++)
+            {
+                int smallest = i;
+                for(int j = i+1; j<unSortedArray.length; j++)
+                {
+                    if(unSortedArray[j] < unSortedArray[smallest])
+                    {
+                        smallest = j;
+                    }
+                    
+                }
+                //Swap
+                int  temp = unSortedArray[smallest];
+                unSortedArray[smallest] = unSortedArray[i];
+                unSortedArray[i] = temp;
+
+            }*/
+
+            //Insertion Sort
+
+            for (int i =1; i<unSortedArray.length; i++ )
+            {
+                int key = unSortedArray[i];
+                int j = i-1;
+                while(j>=0 &&  unSortedArray[j]>key)
+                {
+                    unSortedArray[j+1] = unSortedArray[j];
+                    j--;
+                }
+                unSortedArray[j+1] = key;
+
+            }
+
+        print(unSortedArray);
     
-}
+    
+
 
     
-    
+    }
     }
 
     
